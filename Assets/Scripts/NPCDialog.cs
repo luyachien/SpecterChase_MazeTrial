7644953a6f868cca6fd5807182b21145a9a413ce
luyachien <luya.chien@gmail.com>
2025-03-25 14:10:04 +0800

    改善玩家移動速度及跳躍高度等問題。完善兔子NPC的對話系統(以圖片顯示)。

---
+++ Assets/Scripts/NPCDialog.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCDialogue : MonoBehaviour
+{
+    public GameObject floatingText; // 兔子頭上的小對話框
+    public GameObject dialogueBox; // 畫面中央的大對話框
+    public Text floatingTextUI;
+    public Text dialogueText;
+    public Text interactionText; // "按下 R 鍵" 提示
+    public List<string> dialogues; // 兔子的對話內容
+
+    private int dialogueIndex = 0;
+    private bool isPlayerNear = false;
+    private bool isTalking = false;
+
+    void Start()
+    {
+        StartCoroutine(FloatingTextLoop());
+        dialogueBox.SetActive(false);
+        interactionText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.R))
+        {
+            if (!isTalking)
+            {
+                StartDialogue();
+            }
+            else
+            {
+                NextDialogue();
+            }
+        }
+    }
+
+    IEnumerator FloatingTextLoop()
+    {
+        while (true)
+        {
+            floatingTextUI.text = "請靠近我！";
+            yield return new WaitForSeconds(1.5f);
+            floatingTextUI.text = "我會告訴你玩法！";
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+
+    private void StartDialogue()
+    {
+        isTalking = true;
+        floatingText.SetActive(false);
+        dialogueBox.SetActive(true);
+        dialogueIndex = 0;
+        dialogueText.text = dialogues[dialogueIndex];
+    }
+
+    private void NextDialogue()
+    {
+        dialogueIndex++;
+        if (dialogueIndex < dialogues.Count)
+        {
+            dialogueText.text = dialogues[dialogueIndex];
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    private void EndDialogue()
+    {
+        isTalking = false;
+        dialogueBox.SetActive(false);
+        floatingText.SetActive(true);
+        StartCoroutine(FloatingTextLoop());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+            floatingText.SetActive(false);
+            interactionText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+            interactionText.gameObject.SetActive(false);
+            if (!isTalking)
+            {
+                floatingText.SetActive(true);
+                StartCoroutine(FloatingTextLoop());
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RabbitDialogue.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RabbitDialogue : MonoBehaviour
+{
+    [Header("兔子頭上的對話框 (循環播放 A~C)")]
+    public Image floatingDialogue;  // 兔子頭上顯示的對話框
+    public Sprite[] floatingImages; // 循環顯示 A~C 的圖片
+    private int floatingIndex = 0;
+
+    [Header("畫面中央的對話框 (按 R 播放 D~F)")]
+    public Image mainDialogue;      // 畫面中間顯示的對話框
+    public Sprite[] dialogueImages; // 按 R 切換 D~F 的圖片
+    private int dialogueIndex = 0;
+
+    [Header("玩家偵測範圍")]
+    public GameObject interactText; // "按下 R 鍵" 提示文字
+    private bool isPlayerNear = false;
+    private bool isDialogueActive = false;
+
+    void Start()
+    {
+        // 初始化
+        if (floatingDialogue != null && floatingImages.Length > 0)
+        {
+            StartCoroutine(LoopFloatingDialogue());
+        }
+        if (mainDialogue != null)
+        {
+            mainDialogue.gameObject.SetActive(false); // 隱藏畫面中的對話框
+        }
+        if (interactText != null)
+        {
+            interactText.SetActive(false); // 隱藏 "按下 R" 提示
+        }
+    }
+
+    void Update()
+    {
+        // 玩家按下 R 鍵時，顯示下一張對話框
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.R))
+        {
+            if (!isDialogueActive)
+            {
+                StartDialogue();
+            }
+            else
+            {
+                ShowNextDialogue();
+            }
+        }
+    }
+
+    IEnumerator LoopFloatingDialogue()
+    {
+        while (true)
+        {
+            floatingDialogue.sprite = floatingImages[floatingIndex]; // 切換對話框圖片
+            floatingIndex = (floatingIndex + 1) % floatingImages.Length; // 循環 A~C
+            yield return new WaitForSeconds(2f); // 每 2 秒切換一次
+        }
+    }
+
+    void StartDialogue()
+    {
+        isDialogueActive = true;
+        floatingDialogue.gameObject.SetActive(false); // 隱藏兔子頭上的對話框
+        interactText.SetActive(false); // 隱藏 "按下 R" 提示
+        mainDialogue.gameObject.SetActive(true); // 顯示畫面中央的對話框
+        dialogueIndex = 0;
+        mainDialogue.sprite = dialogueImages[dialogueIndex]; // 顯示第一張 D
+    }
+
+    void ShowNextDialogue()
+    {
+        dialogueIndex++;
+        if (dialogueIndex < dialogueImages.Length)
+        {
+            mainDialogue.sprite = dialogueImages[dialogueIndex]; // 顯示下一張對話
+        }
+        else
+        {
+            EndDialogue(); // 對話結束
+        }
+    }
+
+    void EndDialogue()
+    {
+        isDialogueActive = false;
+        mainDialogue.gameObject.SetActive(false); // 隱藏畫面中央的對話框
+        floatingDialogue.gameObject.SetActive(true); // 重新顯示兔子頭上的對話框
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+            interactText.SetActive(true); // 顯示 "按下 R 鍵"
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+            interactText.SetActive(false); // 隱藏 "按下 R 鍵"
+        }
+    }
+}
