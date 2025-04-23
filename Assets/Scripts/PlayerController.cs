b5d54e423d9356337ae37eeabb6e40bc88160a9f
luyachien <luya.chien@gmail.com>
2025-04-23 11:49:54 +0800

    增加碰到鬼魂的扣血音效。

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,24 @@ public class PlayerController : MonoBehaviour
     private bool isInvincible = false; // 是否處於無敵狀態
     public float invincibilityDuration = 0.25f; // 無敵時間 (0.5秒)
 
+    [Header("音效設定")]
+    public AudioClip DamageSound;
+    public AudioSource audioSource;
+
     void Start()
     {
         heartManager = FindFirstObjectByType<HeartManager>(); // 獲取 HeartManager
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ghost") && !isInvincible) // 只有當玩家不在無敵狀態時才扣血
         {
+            if (audioSource != null && DamageSound != null)
+            {
+                audioSource.PlayOneShot(DamageSound, 1.5f); // 1.5f 調整音量
+            }
             heartManager.TakeDamage(); // 扣一顆愛心
             StartCoroutine(InvincibilityCoroutine()); // 啟動無敵時間
         }
